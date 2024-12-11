32da879a67d7a73a423224e5cad4743219e3bf39
Saransh <saransh@koirala7.com>
2024-12-11 22:10:54 +0545

    separation of concerns

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,13 @@
-import { MdOutlineMail } from "react-icons/md";
-import { IoLockClosedOutline } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-200">
-      <div className="w-96 flex flex-col justify-center bg-white p-10 gap-10 rounded-lg">
-        <h1 className="text-center font-bold text-2xl">Login</h1>
-        <div className="relative ">
-          <MdOutlineMail className="absolute left-3 top-1/2 transform -translate-y-1/2" />
-          <input
-            className="pl-10 border-b-2 p-3 w-full"
-            placeholder="Enter your email"
-            type="email"
-            required
-          />
-        </div>
-        <div className="relative">
-          <IoLockClosedOutline className="absolute left-3 top-1/2 transform -translate-y-1/2" />
-          <input
-            className="pl-10 border-b-2 p-3 w-full"
-            placeholder="Enter your password"
-            type="password"
-            required
-          />
-        </div>
-        <div className="flex gap-2 items-center justify-between">
-          <div className="flex gap-2  items-center">
-            <input type="checkbox" />{" "}
-            <p className="text-black text-sm text-center">Remember Me</p>
-          </div>
-          <Link to="/forgot">
-            <p className="text-blue-500 text-sm text-center">
-              Forgot password?
-            </p>
-          </Link>
-        </div>
-        <button className="bg-blue-500 text-white p-2">Login Now</button>
-        <p className="text-center text-sm">
-          Not a member?
-          <Link to="/signup" className="text-blue-500">
-            SignUp
-          </Link>
-        </p>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
