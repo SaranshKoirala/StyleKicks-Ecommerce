1e16307ec4ecafdd6c2b0f54ce8c46885ffadb5d
Saransh <upwhats498@gmail.com>
2024-12-18 18:00:56 +0545

    Navbar updated

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -14,21 +14,37 @@ function Navbar() {
   return (
     <div className="flex gap-10 justify-between items-center px-10 py-2 text-white bg-black ">
       <img src="/full-logo.png" className="m-0 p-0" width={150} />
-      <ul className="flex gap-10 justify-center items-center  ">
-        <Link to="/">
-          <li className="cursor-pointer">Home</li>
-        </Link>
-        <li className="cursor-pointer">Contact Us</li>
-        <Link to={"/products"}>
-          <li className="cursor-pointer">Products</li>
-        </Link>
-        <Link to={"/login"}>
-          <li>Login</li>
-        </Link>
-        <Link to={"/signup"}>
-          <li>SignUp</li>
-        </Link>
+
+      <ul className="flex gap-10 justify-center items-center w-[50%]  ">
+        {!isOpen && (
+          <>
+            <Link to="/">
+              <li className="cursor-pointer">Home</li>
+            </Link>
+            <li className="cursor-pointer">Contact Us</li>
+            <Link to={"/products"}>
+              <li className="cursor-pointer">Products</li>
+            </Link>
+            <Link to={"/login"}>
+              <li>Login</li>
+            </Link>
+            <Link to={"/signup"}>
+              <li>SignUp</li>
+            </Link>
+          </>
+        )}
+        {isOpen && (
+          <div className="relative w-full">
+            <IoSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-black text-black"
+            />
+          </div>
+        )}
       </ul>
+
       <ul className="flex gap-5 justify-center items-center">
         <li>
           {!isOpen && (
@@ -37,33 +53,14 @@ function Navbar() {
               onClick={handleSearchBtn}
             />
           )}
-
           {isOpen && (
-            <div className="relative w-full">
-              <IoSearch
-                className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer"
-                onClick={handleSearchBtn}
-              />
-              <input
-                type="text"
-                placeholder="Search..."
-                className="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder:text-black text-black"
-              />
-            </div>
+            <button
+              className="bg-gray-200  text-gray-700 font-semibold p-2 rounded-lg shadow-lg transition"
+              onClick={handleSearchBtn}
+            >
+              Cancel
+            </button>
           )}
-          {/* <IoSearch
-            className="absolute text-2xl cursor-pointer"
-            onClick={handleSearchBtn}
-          />
-
-          {isOpen && (
-            <input
-              className="relative placeholder:italic placeholder:text-slate-400 block bg-white w-[200px] border border-slate-300 rounded-md p-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-              placeholder="Search..."
-              type="text"
-              name="search"
-            />
-          )} */}
         </li>
         <li>
           <MdOutlineShoppingCart className="text-2xl cursor-pointer" />
