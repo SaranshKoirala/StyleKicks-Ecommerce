83a331b6b8bd0b431c8b410f6f33a7ca399c3ca1
Saransh <upwhats498@gmail.com>
2024-12-28 18:29:45 +0545

    added the show password feature

--- client/src/page/Login.jsx
+++ client/src/page/Login.jsx
@@ -1,6 +1,15 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { FaEyeSlash } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
 
 function Login() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleShowBtn() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
   return (
     <div className="flex justify-center items-center min-h-screen ">
       <div className="flex flex-col justify-center items-center gap-4 p-10 shadow-2xl rounded-xl">
@@ -18,21 +27,33 @@ function Login() {
             placeholder="Enter your email address"
             type="email"
             id="email"
-            className="border border-gray-300 placeholder:text-gray-400 px-2 py-1 w-96 rounded-md"
+            className="border border-gray-300 placeholder:text-gray-400 px-2 py-1 w-96 rounded-md focus:outline-none"
             required
           />
         </div>
-        <div className=" flex flex-col gap-2 mb-4">
+        <div className="relative flex flex-col gap-2 mb-4">
           <label htmlFor="password" className="font-bold text-blue-900">
             Password
           </label>
           <input
             placeholder="Enter your password"
-            type="password"
+            type={isOpen ? "text" : "password"}
             id="password"
-            className="border border-gray-300 placeholder:text-gray-400 px-2 py-1 w-96 rounded-md"
+            className="border border-gray-300 placeholder:text-gray-400 px-2 py-1 w-96 rounded-md focus:outline-none"
             required
           />
+          {!isOpen && (
+            <FaEyeSlash
+              className="absolute bottom-2.5 right-2.5"
+              onClick={handleShowBtn}
+            />
+          )}
+          {isOpen && (
+            <FaEye
+              className="absolute bottom-2.5 right-2.5"
+              onClick={handleShowBtn}
+            />
+          )}
         </div>
         <button className=" py-1 px-2 text-2xl text-white bg-blue-900 w-96 rounded-md">
           Login
