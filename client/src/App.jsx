56e4c8c1c8165fb670ed87b7b47a6c0b7e698ded
Saransh <upwhats498@gmail.com>
2024-12-29 21:49:56 +0545

    Added the Error page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./page/Login";
 import Register from "./page/Register";
+import Error from "./page/Error";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -29,6 +29,7 @@ route.post("/", async (req, res) => {
     }
 
     const user = await User.create({ name, email, password, cpassword });
+    user.save();
     res.status(201).json({ message: "success", data: user });
   } catch (error) {
     res.status(500).json({ message: "Something went Wrong!!" });
