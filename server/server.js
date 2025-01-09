e6a2d8aaff51a293b58f618f34ff663d2cdbc359
Saransh <upwhats498@gmail.com>
2025-01-09 22:44:24 +0545

    jwt added

--- server/server.js
+++ server/server.js
@@ -3,16 +3,18 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
 const userRoute = require("./routes/userRoute.js");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 dotenv.config();
 
 //parsing the JSON requests
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
-
 app.use("/api/users", userRoute);
 
+//for invalid route
 app.use("/*", (req, res) => {
   console.error("No routes found");
   res.status(404).send("Routes not found!!");
