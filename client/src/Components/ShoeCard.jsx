a60143c9ff86cd0905128f0d8484fe3b9e57f3d3
Saransh <upwhats498@gmail.com>
2025-04-22 10:02:54 +0545

    updated the shoecard

--- client/src/Components/ShoeCard.jsx
+++ client/src/Components/ShoeCard.jsx
@@ -1,23 +1,35 @@
 import PropTypes from "prop-types";
 import useProductStore from "../Store/productStore";
+import { Link } from "react-router-dom";
 
 function ShoeCard({ item }) {
-  const { error } = useProductStore();
+  const { error, setIsOpen, setSearchTerm } = useProductStore();
+
+  function handleLink() {
+    setIsOpen(false);
+    setSearchTerm("");
+  }
+
   if (!item) {
     return <div>Loading...</div>;
   }
   return error ? (
     <div>{error}</div>
   ) : (
-    <div className="flex flex-col">
+    <Link
+      to={`/product/${item._id}`}
+      className="flex flex-col"
+      onClick={handleLink}
+    >
       <img src={item.image} alt="item.name" className="w-40" />
       <p className="font-semibold">{item.name}</p>
-    </div>
+    </Link>
   );
 }
 
 ShoeCard.propTypes = {
   item: PropTypes.shape({
+    _id: PropTypes.number,
     price: PropTypes.number,
     image: PropTypes.string,
     name: PropTypes.string,
